aa83036d1db8cfe29ae2db9a7629b791cb63f2ad
TakanaGamer <2022003586@ifam.edu.br>
2023-08-16 17:00:13 -0400

    first commit

---
+++ src/main/java/edu/ifam/dra/dra1/Dra1Application.java
@@ -0,0 +1,27 @@
+package edu.ifam.dra.dra1;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Dra1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dra1Application.class, args);
+	}
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/test/java/edu/ifam/dra/dra1/Dra1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifam.dra.dra1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dra1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
